17a454357177ac018dd6d62c033d934212cc524c
ayiooo <coderxialuo@gmail.com>
2020-02-28 00:15:55 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,33 @@
-// pages/home/home.js
+// home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Xialuo',
+    age: 18,
+    students: [
+      { id: 101, name: 'kobe', age: 30 },
+      { id: 102, name: 'zhangsan', age: 28 },
+      { id: 103, name: 'lisi', age: 32 },
+      { id: 104, name: 'why', age: 18 },
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick() {
+    // console.log("按钮发生了点击")
+    // 1.错误做法：页面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
+
+// 编程范式：
+// 1.命令式编程：原生（jQuery）操作DOM 一步步给它命令才能达到你想要的的效果
+// 2.声明式编程：三大框架
