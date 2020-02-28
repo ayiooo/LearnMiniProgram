59441ac684f0f8a617f15b664b418616e41bbcc2
ayiooo <coderxialuo@gmail.com>
2020-02-29 01:12:36 +0800

    1-3注册小程序和页面

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
+// 注册一个小程序示例
 App({
-
-  /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
-  onLaunch: function () {
-    
+  globalData: {
+    name: 'coderwhy',
+    age: 18
   },
-
-  /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
-   */
+  // 小程序初始化完成 只执行一次
+  onLaunch: function (options) {
+    // 网络请求 还是比较少的，都是请求某个页面的时候，在对应页面的声明周期函数中去发送请求
+    // 获取用户信息
+    // ------
+    // console.log(options)
+  },
+  // 小程序显示出来时 每次显示都会执行
   onShow: function (options) {
-    
+    // 1.判断小程序进入场景
+    console.log(options.scene)
+    switch (options.scene) {
+      case 1001: 
+        break;
+      case 1005:
+        break;
+      default:
+        break;
+    }
+    // 2.获取用户信息，并且获取到用户信息后，将用户信息传递给服务器
+    wx.getUserInfo({
+      success: function(res) {
+        // console.log(res)
+      }
+    })
   },
-
-  /**
-   * 当小程序从前台进入后台，会触发 onHide
-   */
+  // 小程序隐藏时
   onHide: function () {
     
   },
-
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
+  // 小程序发生错误时
   onError: function (msg) {
     
   }
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,65 @@
 // pages/home/home.js
+// getApp() 调用获取 App() 产生的示例对象
+// const app = getApp()
+// console.log(app)
+// console.log(app.globalData.name)
+// const name = app.globalData.name
+// const age = app.globalData.age
+
+// 注册一个页面
+// 每一个页面也有自己的生命周期
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // ---- 2. 初始化数据 --------
   data: {
-
+    respData: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // ---- 3. 监听wxml中相关的事件 --------
+  handleGetUserInfo(event) {
+    console.log('---------------')
+    console.log(event)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // ---- 1. 监听页面的生命周期函数 --------
+  // 页面被加载出来
+  onLoad() {
+    console.log("onLoad")
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success: (res) => {
+        const data = res.data
+        this.setData({
+          respData: data
+        })
+        console.log(this.data.respData)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 页面显示出来
+  onShow() {
+    console.log("onShow")
+  }, 
+  // 页面初次渲染完成时 回调 只执行一次
+  onReady() {
+    console.log("onReady")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 页面隐藏起来时
+  onHide() {
+    console.log("onHide")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  // 当页面不被需要时卸载页面
+  onUnload() {
+    console.log("onUnload")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  // ---- 4. 监听其他事件 --------
+  // 监听页面的滚动
+  onPageScroll(e) {
+    console.log(e)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // 监听页面滚动到底部
+  onReachBottom() {
+    console.log('页面滚动到底部')
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 下拉加载
+  onPullDownRefresh() {
+    console.log('下拉加载')
   }
 })
