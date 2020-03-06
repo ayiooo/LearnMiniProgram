dffd4f5c650e66c87b3dd4075c806005f5efd0a8
ayiooo <coderxialuo@gmail.com>
2020-03-06 19:27:39 +0800

    1-9展示弹窗 页面分享 小程序登录演示

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
+const TOKEN = 'token'
 App({
-
+  // 对象（放在内存中），小程序关闭掉就销毁了
+  globalData: {
+    token : ''
+  },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
-    
+    // 1. 先从缓存中取出token 如果没有再进行登录
+    const token = wx.getStorageSync(TOKEN)
+    // 2. 判断token是否有值，如果没值进行登录，有值直接用
+    if(token && token.length !== 0) { // 验证token是否过期就可以了
+      // 验证token是否过期
+      this.check_token(token)
+    } else { // 没有token 进行登录操作
+      this.login()
+    }
   },
-
+  check_token(token) {
+    console.log('执行了验证token操作')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: 'POST',
+      header: {
+        token
+      },
+      success: (res) => {
+        console.log(res)
+      },
+      fail: (err) => {
+        console.log(err)
+      }
+    })
+  },
+  login() {
+    console.log("执行了登录操作")
+    // 登录操作
+    wx.login({
+      // 获取到的code只有5分钟的有效期
+      success: (res) => {
+        // 1. 获取code
+        const code = res.code
+        // 2. 将code发送给我们的服务器
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'POST',
+          data: {
+            code
+          },
+          success: (res) => {
+            // 1.取出token
+            const token = res.data.token
+            // 2.将token保存在globalData中
+            this.globalData.token = token
+            // 3.将token进行本地存储
+            wx.setStorageSync(TOKEN, token)
+          }
+        })
+      }
+    })
+  },
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,63 @@
 // pages/home/home.js
 Page({
+  showToast() {
+    wx.showToast({
+      title: '加载中',
+      duration: 2000,
+      icon: "loading",
+      image: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2104351895,3299575660&fm=26&gp=0.jpg",
+      mask: true,
+      success() {
+        console.log("展示弹窗成功")
+      },
+      fail() {
+        console.log("展示弹窗失败")
+      },
+      complete(){
+        console.log("展示弹窗执行完成")
+      }
+    })   
+  },
+  showModal() {
+    wx.showModal({
+      title: 'this title',
+      content: 'this is content',
+      showCancel: false,
+      success(res) {
+        if(res.cancel) {
+          console.log("你点击了取消")
+        }
+        if(res.confirm) {
+          console.log("你点击了确定")
+        }
+      },
+      fail() {
+        console.log("你点了取消")
+      }
+    })
+  },
+  showLoading() {
+    wx.showLoading({
+      title: '加载中',
+    })
+  
+    setTimeout(() => {
+      // 必须手动调用hideLoading方法才会消失
+      wx.hideLoading()
+    }, 3000)
 
+  },
+  showActionSheet() {
+    wx.showActionSheet({
+      itemList: ['A', 'B', 'C'],
+      success(res) {
+        console.log(res.tapIndex)
+      },
+      fail(res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
   /**
    * 页面的初始数据
    */
@@ -61,6 +118,11 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: "hello",
+      imageUrl: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2104351895,3299575660&fm=26&gp=0.jpg",
+      path: "/pages/about/about"
+    }
+    
   }
 })
